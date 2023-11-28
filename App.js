35cff22e22bf3fe2e6e1c8932c03d14df5e035fd
hector <hpulido1021@gmail.com>
2023-11-28 10:35:09 -0500

    Feature: created map method to render weather data on weathercard component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-// Import data and WeatherCard here
+import cities from './data';
+import WeatherCard from './components/WeatherCard'
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {/* Render components here */}
+                {cities.map((weatherData) => <WeatherCard weather = {weatherData}/>)}
                 
 
 
--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,20 +1,20 @@
 import React from "react";
-import sunny from "../assets/Sunny.svg"
+import sunny from "../assets/sunny.svg"
 
 
-function WeatherCard() {
+function WeatherCard(props) {
   return (
     <div className = "card">
         <div className = "img-container">
             <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
-            <h3 className="card-title">CITY DATA HERE</h3>
-            <h5 className="card-text">TEMPERATURE DATA HERE</h5>
-            <h5 className="card-text">FORECAST DATA HERE</h5>
+            <h3 className="card-title">{props.weather.city}</h3>
+            <h5 className="card-text">{props.weather.temperature}</h5>
+            <h5 className="card-text">{props.weather.forecast}</h5>
         </div>
     </div>
   );
 };
 
-// Export the WeatherCard
+module.exports = WeatherCard;
